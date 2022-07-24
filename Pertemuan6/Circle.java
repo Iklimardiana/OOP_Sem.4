f19ca7660b800fc6c5949813166c9c2442d5ebe7
Iklimardiana <81839323+Iklimardiana@users.noreply.github.com>
2022-07-24 08:25:45 +0700

    Add files via upload

---
+++ Pertemuan6/Circle.java
@@ -0,0 +1,11 @@
+package Pertemuan6;
+
+public class pert6_Circle {
+    //properties circle
+    //radius of circle
+    double radius =1;
+
+    //constructor
+    //construct a circle object
+    
+}
